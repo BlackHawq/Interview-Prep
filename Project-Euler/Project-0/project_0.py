8a4c2fba1703a163a1d1a60b5b2c7e0c5abd0d65
Hammud Haq <haq.hammud@gmail.com>
2026-09-02 16:48:25 -0700

    Project-0 Euler

---
+++ Project-Euler/Project-0/project_0.py
@@ -0,0 +1,8 @@
+#Calculate the Sum of odd squares up to n
+
+def sum_of_odd_squares(n):
+    total = 0
+    for i in range(1, n + 1):
+        if i % 2 != 0:  # Check if the number is odd
+            total += i ** 2  # Add the square of the odd number to the total
+    return total
