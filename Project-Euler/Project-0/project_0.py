b6fbf1339836ce4a929c9a26420d61e3e06986e9
Hammud Haq <haq.hammud@gmail.com>
2026-09-02 16:53:36 -0700

    Adding input and wait for result

--- Project-Euler/Project-0/project_0.py
+++ Project-Euler/Project-0/project_0.py
@@ -6,3 +6,8 @@ def sum_of_odd_squares(n):
         if i % 2 != 0:  # Check if the number is odd
             total += i ** 2  # Add the square of the odd number to the total
     return total
+
+n = int(input("Enter a number: "))
+
+print(sum_of_odd_squares(n))
+input("Press Enter to exit...")
